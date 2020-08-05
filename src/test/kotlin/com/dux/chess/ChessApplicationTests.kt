f9fe74c4eefd7dbf5a400b1758a6aad0b6970d1f
Lucas Vinicius Salviano Rodrigues <lucas.vinicius.rodrigues@hotmail.com>
2020-08-05 06:02:14 -0300

    Adding .gitignore

---
+++ src/test/kotlin/com/dux/chess/ChessApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dux.chess
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChessApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
